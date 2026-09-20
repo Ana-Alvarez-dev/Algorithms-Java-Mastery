560e30f94fc5a7085ed57b0600dfa5da0d271a46
a-dev-backend <ana.alvarez.utn@gmail.com>
2026-09-20 11:23:12 -0300

    feat(arrays): add ArrayAccess implementation.

---
+++ src/main/java/org/anaalvarezdev/algorithms/arrays/ArrayAccess.java
@@ -0,0 +1,50 @@
+package org.anaalvarezdev.algorithms.arrays;
+
+public class ArrayAccess {
+    /**
+     * Problem:
+     * Given an integer array and a valid index,
+     * return the element stored at that position.
+     *
+     * Input:
+     * - An integer array.
+     * - A valid index.
+     *
+     * Output:
+     * - The value stored at the specified index.
+     *
+     * Precondition:
+     * - 0 <= index < array.length
+     *
+     * Postcondition:
+     * - The returned value is equal to array[index].
+     *
+     * Algorithmic Strategy:
+     * Direct indexed access.
+     *
+     * Time Complexity:
+     * - Θ(1)
+     *
+     * Space Complexity:
+     * - Θ(1)
+     */
+
+    public final class ArrayAccess {
+
+        private ArrayAccess() {
+
+        }
+
+        public static int get(int[] array, int index) {
+            /**
+             * This method accesses an array and returns the value stored at the specified index.
+             *
+             * @param array The integer array to access.
+             * @param index The valid index of the element to retrieve.
+             * @return The value stored at the specified index.
+             */
+            return array[index];
+        }
+    }
+
+}
